10d37d340dd575c20e3a31c29fad877a7fc47d91
william <willhgreen02@gmail.com>
2022-01-09 09:43:59 -0700

    commit tree ffbeaec993516ad7de776b454f241b16adb08bcb

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
 }
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
+function isPalindrome(word) {
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
+}
 
 /* 
   Add your pseudocode here
+iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
 */
 
 /*
@@ -12,9 +25,28 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
